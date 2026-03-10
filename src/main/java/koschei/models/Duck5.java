874d39e7b28893693bf5232aa6e093c8686f7a27
Vladislav Gutnik <lt38igreen@gmail.com>
2026-03-10 18:33:23 +0300

    Job done

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,13 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Duck5 {
+    private Egg6 egg;
 
-    @Override
-    public String toString() {
-        return ", в утке яйцо " + "";
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
+    public String getDuck5Says() {
+        return ", в утке яйцо " + egg.toString();
     }
 }
